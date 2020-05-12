f5077a736edc25f2843a6c8e71f021bb55175776
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-05-13 02:13:08 +0500

    almost done

--- a.c
+++ a.c
@@ -7,7 +7,6 @@
 
 int main(void) {
 
-	char inbuf[MSGSIZE];
 	pid_t childpid, childpid2;
 	int fd[2];
 
@@ -17,7 +16,7 @@ int main(void) {
 	}
 
 	if (childpid == 0) { 				//Child
-		//printf("Inside the first child\n");
+		
 		if (dup2(fd[1], STDOUT_FILENO) == -1){
 			perror("Failed to redirect stdout of tail");
 		}
@@ -26,8 +25,7 @@ int main(void) {
 			perror("Failed to close extra pipe descriptors on tail");
 		}	
 		
-		else {
-			//printf("About to run tail\n");
+		else {			
 			execl("/usr/bin/tail", "tail", "-5", "b.txt", NULL);
 			perror("Failed to exec tail");
 		}
@@ -41,18 +39,20 @@ int main(void) {
 
 		if(childpid2 == 0){
 			printf("In child2\n");
-			if (dup2(fd[1], STDOUT_FILENO) == -1){
-				perror("Failed to redirect stdout of grep");
+
+			if (dup2(fd[0], STDIN_FILENO) == -1){
+				perror("Failed to redirect stdout of tail");
 			}
 		
 			else if ((close(fd[0]) == -1) || (close(fd[1]) == -1)){
 				perror("Failed to close extra pipe descriptors on grep");
 			}	
 			
-			else {
-				execl("/usr/bin/grep", "grep", "ee", NULL);
+			else{
+				execl("/usr/bin/grep", "grep", "is", NULL);
 				perror("Failed to exec grep");
 			}
+			
 			return 1;
 		}
 
@@ -61,18 +61,17 @@ int main(void) {
 			wait(NULL);
 			printf("In parent\n");
 
-			if (dup2(fd[0], STDIN_FILENO) == -1){ /* sort is the parent */
-				perror("Failed to redirect stdin of sort");
-			}
-			else if ((close(fd[0]) == -1) || (close(fd[1]) == -1)){
-				perror("Failed to close extra pipe file descriptors on sort");
-			}
-			else {
-				read(fd[0], inbuf, MSGSIZE);
-				printf("%s\n", inbuf);
-				//execl("/usr/bin/sort", "sort", NULL);
-				//perror("Failed to exec sort");
-			}
+			// read(fd2[0], inbuf2, MSGSIZE);
+			// printf("%s\n", inbuf2);
+
+			// if ((close(fd2[0]) == -1) || (close(fd2[1]) == -1)){
+			// 	perror("Failed to close extra pipe file descriptors on sort");
+			// }
+			// else {
+				
+			// 	execl("/usr/bin/sort", "sort", inbuf2, NULL);
+			// 	perror("Failed to exec sort");
+			// }
 			return 1;
 		}
 	}
