5687960d0f17d83e20e8ec2ca6e727c3df1b94af
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-05-13 02:18:53 +0500

    .

--- a.c
+++ a.c
@@ -40,18 +40,18 @@ int main(void) {
 		if(childpid2 == 0){
 			printf("In child2\n");
 
-			if (dup2(fd[0], STDIN_FILENO) == -1){
-				perror("Failed to redirect stdout of tail");
-			}
+			// if (dup2(fd[0], STDIN_FILENO) == -1){
+			// 	perror("Failed to redirect stdout of tail");
+			// }
 		
-			else if ((close(fd[0]) == -1) || (close(fd[1]) == -1)){
-				perror("Failed to close extra pipe descriptors on grep");
-			}	
+			// else if ((close(fd[0]) == -1) || (close(fd[1]) == -1)){
+			// 	perror("Failed to close extra pipe descriptors on grep");
+			// }	
 			
-			else{
-				execl("/usr/bin/grep", "grep", "is", NULL);
-				perror("Failed to exec grep");
-			}
+			// else{
+			// 	execl("/usr/bin/grep", "grep", "is", NULL);
+			// 	perror("Failed to exec grep");
+			// }
 			
 			return 1;
 		}
@@ -61,17 +61,19 @@ int main(void) {
 			wait(NULL);
 			printf("In parent\n");
 
-			// read(fd2[0], inbuf2, MSGSIZE);
-			// printf("%s\n", inbuf2);
-
-			// if ((close(fd2[0]) == -1) || (close(fd2[1]) == -1)){
-			// 	perror("Failed to close extra pipe file descriptors on sort");
-			// }
-			// else {
+			if (dup2(fd[0], STDIN_FILENO) == -1){
+				perror("Failed to redirect stdout of tail");
+			}
+		
+			else if ((close(fd[0]) == -1) || (close(fd[1]) == -1)){
+				perror("Failed to close extra pipe descriptors on grep");
+			}	
+			
+			else{
 				
-			// 	execl("/usr/bin/sort", "sort", inbuf2, NULL);
-			// 	perror("Failed to exec sort");
-			// }
+				execl("/usr/bin/sort", "sort", NULL);
+				perror("Failed to exec sort");
+			}
 			return 1;
 		}
 	}
